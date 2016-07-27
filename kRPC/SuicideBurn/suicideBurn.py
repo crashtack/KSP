be499f236f1367646d9bf4647c0bb67936ae4b99
crashtack <crashtack@gmail.com>
2016-07-27 13:32:23 -0700

    trying to fix mun landing bugs

--- kRPC/SuicideBurn/suicideBurn.py
+++ kRPC/SuicideBurn/suicideBurn.py
@@ -12,15 +12,27 @@ conn = krpc.connect(name='Launch Science Station to Orbit')
 vessel = conn.space_center.active_vessel
 altitude = conn.add_stream(getattr, vessel.flight(), 'mean_altitude')
 
-def suicide_burn_calc(vessel,saf,g,thrust,vv):
+def suicide_burn_calc(vessel,saf,g,thrust,vs):
     safety      = saf          # 1 corresponds to no safety margin, 1.1 had 10% safety margin
     shipmass    = vessel.mass
     va = ((thrust/shipmass) + g)
-    a = (vv**2)/(2*va)
+    a = (vs**2)/(2*va)
     return safety * a
 
-def print_status(h, throttle, burn_height, burn_time, vv):
-    message = ("Height: %.2f SNH: %.1f Throttle: %.2f Burn Time: %.2f Vertical Velocity: %.2f" % (h,burn_height,throttle,burn_time,vv))
+def suicide_burn_height(vessel,saf,g,thrust,vs):
+    shipmass = vessel.mass
+    a = (thrust/shipmass) - g
+    t = vs / a
+    h = (0.5*(vs**2))/a
+    return h
+
+def surface_velocity(vt):
+    Vab = ((vt[0]**2)+(vt[1]**2))**.5
+    Vs  = ((vt[2]**2)+(Vab**2))**.5
+    return Vs
+
+def print_status(h, throttle, burn_height, burn_time, vs):
+    message = ("Height: %.2f SBH: %.1f Throttle: %.2f Burn Time: %.2f Vertical Velocity: %.2f  " % (h,burn_height,throttle,burn_time,vs))
     sys.stdout.write("\r" + message)
     sys.stdout.flush()
 
@@ -36,36 +48,46 @@ ap.reference_frame = vessel.orbital_reference_frame
 ap.engage()
 ap.disengage()
 g = vessel.orbit.body.surface_gravity
+print('Orbital Body: ', vessel.orbit.body.name)
+print('Gravity = ', g)
 
 time.sleep(1)
 vessel.control.sas = True
 
 """ Set Parameters for Landing """
-safety = .90
+safety = 1.1
 vLand = -8      # landing speed m/s
 runmode = 1     # ??
 thrust = vessel.max_thrust
+print('Max Thrust = ', thrust)
 counter = 0     # for checking if landed
 
 """ Find ship parameters """
+shipmass    = vessel.mass
+print('Mass = ', shipmass)
 h = vessel.flight(vessel.orbit.body.reference_frame).surface_altitude
 velocity_tupple = vessel.velocity(vessel.orbit.body.reference_frame)
-vv = -velocity_tupple[2]
-vDiff = vLand - vv
-
-""" Calculates values for landing """
-
+vs = surface_velocity(velocity_tupple)      # surface Velocity
+vv = -velocity_tupple[2]                    #vertical velocity
+vDiff = vLand - vs
+SBH = suicide_burn_height(vessel,safety,g,thrust,vs)
+print('SBH = ', SBH)
+a = (thrust/shipmass) - g
+t = vs / a
+dV = t * thrust / 1000
+print('Delta V: ', dV)
 
 """ Loop until landed """
 while runmode:
     h = vessel.flight(vessel.orbit.body.reference_frame).surface_altitude
     velocity_tupple = vessel.velocity(vessel.orbit.body.reference_frame)
+    vs = -surface_velocity(velocity_tupple)      # surface Velocity
     vv = -velocity_tupple[2]
-    vDiff = vLand - vv
+    vDiff = vLand - vs
 
     """ Calculates values for landing """
-    SBH = suicide_burn_calc(vessel,safety,g,thrust,vv)     # Calculates the suicide burn Height
-    time_to_burn = ((SBH - h)/vv)
+    SBH = suicide_burn_calc(vessel,safety,g,thrust,vs)     # Calculates the suicide burn Height
+    time_to_burn = ((SBH - h)/vs)
     zeroAccelTrust = ((vessel.mass * g)/thrust)
 
 
@@ -85,7 +107,7 @@ while runmode:
 
     """ Suicide burn start """
     if runmode == 2:
-        if h < (SBH - vv):
+        if h < (SBH - vs):
         #if h < SBH:
             vessel.control.throttle = 1
             vessel.control.rcs = False
@@ -97,13 +119,13 @@ while runmode:
     if runmode == 3:
         #else:
             #vessel.control.throttle = 0
-        #if h/-vv < 2:
-            #print('h/-vv')
+        #if h/-vs < 2:
+            #print('h/-vs')
             # gear on
             # brakes off
-        if vv > -6:
+        if vs > -6:
             print()
-            print('vv is now less then -6 ms, switching to final approach')
+            print('vs is now less then -6 ms, switching to final approach')
             print('SAS Mode: ', ap.sas_mode)
             #ap.disengage()
             #vessel.control.sas = True
@@ -120,13 +142,13 @@ while runmode:
         elif vDiff < 0:
             vessel.control.throttle = zeroAccelTrust - 0.01
 
-        if vv > -0.1 and h < 100:
+        if vs > -0.1 and h < 100:
             counter += 1
             time.sleep(.1)
         if counter > 20:
             runmode = 0     # if landed for 2 seconde then exit
 
-    print_status(h, vessel.control.throttle, SBH, time_to_burn, vv)
+    print_status(h, vessel.control.throttle, SBH, time_to_burn, vs)
     time.sleep(0.05)
 
 """ Landing notification and program exit """
